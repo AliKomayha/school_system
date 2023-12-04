2366d2320bae2709a46a80bd4748496a0e064ec5
alli komayha <Ali Komayha>
2023-12-04 19:34:15 +0200

    registration post

--- register.php
+++ register.php
@@ -1,3 +1,72 @@
+<?php
+    include "connection.php";
+
+    $conn = mysqli_connect("localhost", "root", "", "school_system");
+
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    // else{
+    //     echo"tam";
+    // }
+    
+
+    // security function 
+    function escapehtmlchars($input){
+        return htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
+    }
+
+
+    //handle registration from form
+    if($_SERVER["REQUEST_METHOD"]=="POST"){
+        $username= isset($_POST["username"])? mysqli_real_escape_string($conn, $_POST["username"]): '';
+        $password= isset($_POST["password"])? mysqli_real_escape_string($conn, $_POST["password"]): '';
+        $role=isset($_POST["role"])? mysqli_real_escape_string($conn, $_POST["role"]): '';
+
+        // security feature
+        $username = escapehtmlchars($username);
+        $password = escapehtmlchars($password);
+        $role = escapehtmlchars($role);
+
+        //security feature 2 
+        $username=substr($username, 0, 20);
+        $password=substr($password, 0, 20);
+        $password= md5($password); // hashing the password
+
+        if(empty($username) || empty($password)){
+            echo"Username and password cannot be empty.";
+        }
+        else{
+            $validRoles= array('teacher','student');
+            if(!in_array($role,$validRoles)){
+                echo"Invalid role";
+            }
+            else{
+                $sql=" INSERT INTO users (id, username, password, role) VALUES (NULL,'$username', '$password','$role') ";
+
+
+                
+            }
+            if(mysqli_query($conn,$sql)){
+                echo"Registartion successful";
+            }
+            else{
+                echo "Error: ". mysqli_error($conn);
+            }
+        }
+
+    }
+    closeDBconnection($conn);
+
+?>
+
+
+
+
+
+
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -16,7 +85,7 @@
     <input type="text" name="username" required><br>
 
     <label for="password"> Password: </label>
-    <input type="text" name="password" required><br>
+    <input type="password" name="password" required><br>
 
     <label for="role"> Role: </label>
     <select name="role"required>
